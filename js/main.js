ebe303023bf498e2272d96a458b5ae17caa8a06c
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-04-29 15:28:23 +0500

    comment desktop the end

--- js/main.js
+++ js/main.js
@@ -9,3 +9,43 @@ footer_btn.addEventListener('click', () => {
     footer_text.classList.toggle('active')
 })
 // footer
+
+// comment
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
+let swiper = new Swiper(".comment_Swiper", {
+//   centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  on: {
+    autoplayTimeLeft(s, time, progress) {
+      progressCircle.style.setProperty("--progress", 1 - progress);
+      progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+    }
+  },
+  breakpoints: {
+    300: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+  },
+});
+// comment
