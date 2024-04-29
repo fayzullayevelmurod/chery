aabb7c6416028d46d6136ea4685fa3cb3bd168cb
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-04-29 14:46:42 +0500

    footer desktop the end

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+// footer
+let footer_btn = document.querySelector('.footer_btn');
+let footer_text = document.querySelector('.footer_text');
+
+footer_btn.addEventListener('click', () => {
+    footer_btn.classList.toggle('active')
+    footer_text.classList.toggle('active')
+})
+// footer
